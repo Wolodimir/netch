e072c08e906de76eca1c3b244282f219d63a42f1
Wolodimir <dvdent666@gmail.com>
2021-03-21 20:42:14 +0400

    No need permission auth. Use just roles

--- src/main/java/com/team/netch/security/AppUserRole.java
+++ src/main/java/com/team/netch/security/AppUserRole.java
@@ -1,23 +1,6 @@
 package com.team.netch.security;
 
-import com.google.common.collect.Sets;
-
-import java.util.Set;
-
 public enum AppUserRole {
-    STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(AppUserPermission.COURSE_READ,
-            AppUserPermission.COURSE_WRITE,
-            AppUserPermission.STUDENT_READ,
-            AppUserPermission.STUDENT_WRITE));
-
-    private final Set<AppUserPermission> permissions;
-
-    AppUserRole(Set<AppUserPermission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public Set<AppUserPermission> getPermissions() {
-        return permissions;
-    }
+    STUDENT,
+    ADMIN
 }
