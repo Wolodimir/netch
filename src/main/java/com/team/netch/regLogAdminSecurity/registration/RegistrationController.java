713c1da0d09911c5d5c7002755ed7ed959d3ea27
Wolodimir <dvdent666@gmail.com>
2021-03-29 22:39:58 +0400

    Change cors policy

--- src/main/java/com/team/netch/regLogAdminSecurity/registration/RegistrationController.java
+++ src/main/java/com/team/netch/regLogAdminSecurity/registration/RegistrationController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/registration")
+@CrossOrigin
 public class RegistrationController {
 
     private final RegistrationService registrationService;
--- src/main/java/com/team/netch/regLogAdminSecurity/security/config/WebSecurityConfig.java
+++ src/main/java/com/team/netch/regLogAdminSecurity/security/config/WebSecurityConfig.java
@@ -40,6 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
